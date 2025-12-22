b32c832eefddd1d413a5bd75bbdbfef01644c144
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-12-21 22:25:09 -0500

    Added telemetry models

--- telemetry/admin.py
+++ telemetry/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Trace, Observation, Generation
 
-# Register your models here.
+admin.site.register(Trace)
+admin.site.register(Observation)
+admin.site.register(Generation)
