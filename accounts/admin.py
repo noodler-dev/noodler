fcead9e60fe639be0bf300181c1778e46475d042
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-12-25 17:56:04 -0500

    Added membership models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Organization, UserProfile
+from .models import Organization, UserProfile, Membership
 
 admin.site.register(Organization)
 admin.site.register(UserProfile)
+admin.site.register(Membership)
--- accounts/models.py
+++ accounts/models.py
@@ -16,3 +16,18 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Membership(models.Model):
+    ROLE_CHOICES = [
+        ("admin", "Admin"),
+        ("member", "Member"),
+    ]
+
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_profile.user.username} - {self.organization.name}"
