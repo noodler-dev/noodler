855d1483be7720e78f90e8b7c6afbf0ac7dfd211
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-12-25 18:00:21 -0500

    Added projects app

---
+++ projects/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from accounts.models import Organization
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class ApiKey(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    hashed_key = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    revoked_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.hashed_key
