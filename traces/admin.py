705d197877d01deb0404156c8891fe7c7d36c559
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-12-29 22:13:35 -0500

    Added Span model

--- traces/admin.py
+++ traces/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Trace
+from .models import Trace, Span
 
 admin.site.register(Trace)
+admin.site.register(Span)
--- traces/models.py
+++ traces/models.py
@@ -4,11 +4,24 @@ from projects.models import Project
 
 
 class Trace(models.Model):
-    trace_id = models.CharField(max_length=32)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    trace_id = models.CharField(max_length=32)
     started_at = models.DateTimeField()
     ended_at = models.DateTimeField()
     metadata = JSONField()
 
     def __str__(self):
         return self.trace_id
+
+
+class Span(models.Model):
+    trace = models.ForeignKey(Trace, on_delete=models.CASCADE)
+    span_id = models.CharField(max_length=16)
+    parent_span_id = models.CharField(max_length=16, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True, blank=True)
+    attributes = JSONField()
+
+    def __str__(self):
+        return self.name
