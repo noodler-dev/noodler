c57c72fb9ed617ff14643ee89a50c2069ea99d94
Nawaz Gafar <gafar.nawaz@gmail.com>
2026-01-01 17:14:56 -0500

    Added payload_protobuf field

--- traces/models.py
+++ traces/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.db.models import JSONField
+from django.db.models import JSONField, BinaryField
 from projects.models import Project
 
 
 class RawTrace(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)    
+    payload_json = JSONField()
+    payload_protobuf = BinaryField(blank=True, null=True)
     received_at = models.DateTimeField()
-    payload = JSONField()
 
     def __str__(self):
         return f"RawTrace for {self.project.name}"
