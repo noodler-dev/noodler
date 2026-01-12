386600b4f0d14ce0ea59c01ef3a54007e44d938a
Nawaz Gafar <gafar.nawaz@gmail.com>
2026-01-11 20:30:47 -0500

    Created account views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import UserProfile
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("accounts:login")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Create UserProfile for the new user
+        UserProfile.objects.create(user=self.object)
+        return response
+
+
+class CustomLoginView(LoginView):
+    template_name = "accounts/login.html"
+    redirect_authenticated_user = True
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy("accounts:login")
