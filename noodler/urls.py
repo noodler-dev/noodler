7cb1586897488e9e8ddc8e97fce3d501b37a376a
Nawaz Gafar <gafar.nawaz@gmail.com>
2026-01-11 21:18:22 -0500

    Added views for projects

--- noodler/urls.py
+++ noodler/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("projects/", include("projects.urls")),
     path("api/traces/", include("traces.api.urls")),
 ]
---
+++ projects/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'projects'
+
+urlpatterns = [
+    path('', views.project_list, name='list'),
+    path('new/', views.project_create, name='create'),
+    path('<int:project_id>/', views.project_detail, name='detail'),
+    path('<int:project_id>/edit/', views.project_edit, name='edit'),
+    path('<int:project_id>/delete/', views.project_delete, name='delete'),
+    path('<int:project_id>/switch/', views.project_switch, name='switch'),
+    path('<int:project_id>/keys/create/', views.api_key_create, name='key_create'),
+    path('<int:project_id>/keys/<int:key_id>/created/', views.api_key_created, name='key_created'),
+    path('<int:project_id>/keys/<int:key_id>/revoke/', views.api_key_revoke, name='key_revoke'),
+]
