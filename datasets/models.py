a8d27c912c46d96127d0de9fb60b09c271e3b32e
Nawaz Gafar <gafar.nawaz@gmail.com>
2026-01-28 20:17:32 -0500

    make clean

--- datasets/models.py
+++ datasets/models.py
@@ -32,12 +32,16 @@ class Dataset(models.Model):
 
     def get_first_unannotated_trace(self):
         """Get the first trace in this dataset that hasn't been annotated yet."""
-        annotated_trace_ids = Annotation.objects.filter(dataset=self).values_list("trace_id", flat=True)
+        annotated_trace_ids = Annotation.objects.filter(dataset=self).values_list(
+            "trace_id", flat=True
+        )
         return self.get_traces_ordered().exclude(id__in=annotated_trace_ids).first()
 
     def get_unannotated_count(self):
         """Return the number of traces that haven't been annotated yet."""
-        annotated_trace_ids = Annotation.objects.filter(dataset=self).values_list("trace_id", flat=True)
+        annotated_trace_ids = Annotation.objects.filter(dataset=self).values_list(
+            "trace_id", flat=True
+        )
         return self.traces.exclude(id__in=annotated_trace_ids).count()
 
     class Meta:
